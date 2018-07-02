495f5170462c567b37e20c8cf31f1e666ea38740
VladBezgin <39759704+VladBezgin@users.noreply.github.com>
2018-07-03 00:40:52 +0400

    Update script.js

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -228,3 +228,28 @@ let lovers = document.querySelectorAll('.lovers'),
 	portNo(granddad);
 	portNo(grandmother);
 
+function consultTime() {
+	if(popupDesign.style.display == 'none' || popupGift.style.display == 'none') {
+	popupDialog0.addEventListener('mouseenter', () => isMouseLeft0 = false); 
+    popupDialog0.addEventListener('mouseleave', () => isMouseLeft0 = true);
+
+        popupConsult.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    
+
+
+    popupConsult.addEventListener('click', () => {
+        if (isMouseLeft0) {
+            popupConsult.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    });
+
+    close0.addEventListener('click', function() {
+        popupConsult.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+};
+};
+
+setTimeout(consultTime, 60000);
