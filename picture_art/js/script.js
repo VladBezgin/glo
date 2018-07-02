19b04a995f60dba454d5092dc51233e1e889eabb
VladBezgin <39759704+VladBezgin@users.noreply.github.com>
2018-07-03 00:14:39 +0400

    Update script.js

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -155,7 +155,15 @@ let stylesBtn = document.getElementsByClassName('button-styles')[0],
 
 stylesBtn.addEventListener('click', () => {
 	for(i = 0; i < styles2.length; i++){
-    styles2[i].style.display = 'block';
+    styles2[i].classList.remove('hidden-lg');
+    styles2[i].classList.remove('hidden-md');
+    styles2[i].classList.remove('hidden-sm');
+    styles2[i].classList.remove('hidden-xs');
+    styles2[i].classList.add('col-sm-3');
+    styles2[i].classList.add('col-sm-offset-0');
+    styles2[i].classList.add('col-xs-10');
+    styles2[i].classList.add('col-xs-offset-1');
+
 }
     stylesBtn.style.display = 'none';
 });
