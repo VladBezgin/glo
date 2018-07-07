6931122d82881fb34583b9f177383d9400ddcac1
VladBezgin <39759704+VladBezgin@users.noreply.github.com>
2018-07-07 10:56:13 +0400

    Add files via upload

--- picture_art/js/parts/modalDesign.js
+++ picture_art/js/parts/modalDesign.js
@@ -1,9 +1,27 @@
 function modalDesign() {
     let designBtn = document.querySelectorAll('.button-design'),
-        close2 = document.getElementsByClassName('popup-close')[2],
-        popupDialog2 = document.getElementsByClassName('popup-dialog')[2],
-        popupDesign = document.getElementsByClassName('popup-design')[0],
-        isMouseLeft2 = true;
+                close2 = document.getElementsByClassName('popup-close')[2],
+                popupDialog2 = document.getElementsByClassName('popup-dialog')[2],
+                popupDesign = document.getElementsByClassName('popup-design')[0],
+                isMouseLeft2 = true,
+                inputDesign = document.querySelectorAll('.inputDesign'),
+                inputConsulation = document.querySelector('.inputConsulation'),
+                inputConsult = document.querySelectorAll('.inputConsult');
+
+function rusInput(input) {
+input.addEventListener('input', function(e) {
+  let res = /[^а-яёЪА-ЯЁЪ]/g.exec(this.value);
+  this.value = this.value.replace(res, '');
+});
+};
+
+rusInput(inputDesign[0]);
+rusInput(inputDesign[3]);
+rusInput(inputConsulation[0]);
+rusInput(inputConsult[0]);
+rusInput(inputConsult[3]);
+
+
 
     function mdalOpenDesign(btn) {
 
